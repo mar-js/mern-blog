7be4f31802417f8653306f880191cf94814c2e8e
Martin <maguera37@gmail.com>
2024-01-20 21:13:32 -0300

    fix: correction alias paths in vite and ts config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	resolve: {
 		alias: {
-			"@src": path.resolve(__dirname, "./"),
-			"@pages": path.resolve(__dirname, "pages/"),
+			"@": path.resolve(__dirname, "src"),
 		},
 	},
 	plugins: [react()],
