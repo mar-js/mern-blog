11369164ac22a8f4c150d4be53da7f56df3bc85b
Martin <maguera37@gmail.com>
2024-01-21 20:03:00 -0300

    feat: change style of title componentt

--- client/src/components/title/index.tsx
+++ client/src/components/title/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Link } from "wouter";
 
 export const Title: FC = () => (
-	<h1 className="w-96 text-3xl font-bold text-blue-400">
+	<h1 className="w-80 text-3xl font-bold text-blue-400">
 		<Link href="/">MERN Blog</Link>
 	</h1>
 );
