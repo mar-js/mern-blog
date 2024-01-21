36f174b0fbbff0a9678d67a61a1fe248048d2de3
Martin <maguera37@gmail.com>
2024-01-20 21:55:26 -0300

    feat: add links const to navbar component

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,3 +1,13 @@
+import { links } from "@/utils";
 import { FC } from "react";
+import { Link } from "wouter";
 
-export const Navbar: FC = () => <nav>Nav</nav>;
+export const Navbar: FC = () => (
+	<nav>
+		{links.map(({ id, text, link }) => (
+			<Link key={id} href={link}>
+				<b>{text}</b>
+			</Link>
+		))}
+	</nav>
+);
