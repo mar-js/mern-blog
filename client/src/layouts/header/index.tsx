6508ad4f1acbc967b45892ee50a1c3184224fe0b
Martin <maguera37@gmail.com>
2024-01-20 22:59:31 -0300

    feat: add style to header layout

--- client/src/layouts/header/index.tsx
+++ client/src/layouts/header/index.tsx
@@ -2,7 +2,7 @@ import { Navbar, Title } from "@/components";
 import { FC } from "react";
 
 export const Header: FC = () => (
-	<header>
+	<header className="flex items-center w-full py-2 px-8 border-b-2 mb-10">
 		<Title />
 		<Navbar />
 	</header>
