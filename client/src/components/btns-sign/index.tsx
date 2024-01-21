34fe59ae88167ff3abea3dd094b87accac8d6c62
Martin <maguera37@gmail.com>
2024-01-21 20:03:00 -0300

    feat: change style of btns-sign componentt

--- client/src/components/btns-sign/index.tsx
+++ client/src/components/btns-sign/index.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react";
+import { Link } from "wouter";
 
 export const BtnsSign: FC = () => (
 	<div className="flex justify-center items-center font-bold text-gray-400">
-		<button type="button" className="">
+		<Link href="/sign-in" className="ml-4 hover:opacity-70">
 			Sign In
-		</button>
-		<button type="button" className="ml-4 border rounded-2xl py-2 px-4">
+		</Link>
+		<Link
+			href="/sign-up"
+			className="ml-4 border-2 rounded-2xl py-2 px-4 hover:opacity-70"
+		>
 			Sign Up
-		</button>
+		</Link>
 	</div>
 );
