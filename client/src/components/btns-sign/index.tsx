152551fc75a241cdd0d1446f7ca861a218261c0f
Martin <maguera37@gmail.com>
2024-01-20 22:58:22 -0300

    feat: add style to btns-sign component

--- client/src/components/btns-sign/index.tsx
+++ client/src/components/btns-sign/index.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
 
 export const BtnsSign: FC = () => (
-	<div>
-		<button type="button">Sign In</button>
-		<button type="button">Sign Up</button>
+	<div className="flex justify-center items-center font-bold text-gray-400">
+		<button type="button" className="">
+			Sign In
+		</button>
+		<button type="button" className="ml-4 border rounded-2xl py-2 px-4">
+			Sign Up
+		</button>
 	</div>
 );
