bae196542bf8f661c82e32ac7783076397704d73
Martin <maguera37@gmail.com>
2024-01-20 22:58:47 -0300

    feat: add style to navbar component

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -2,7 +2,7 @@ import { BtnsSign, Links, Search, SwitchColorMode } from "@/components";
 import { FC } from "react";
 
 export const Navbar: FC = () => (
-	<nav>
+	<nav className="flex w-full justify-between items-center">
 		<Search />
 		<Links />
 		<SwitchColorMode />
