941dda32d5e187cf1cff2960e94fc0fa725cefd6
Martin <maguera37@gmail.com>
2024-01-20 21:59:55 -0300

    feat: add links component in navbar component

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,15 +1,9 @@
-import { Search } from "@/components";
-import { links } from "@/utils";
+import { Links, Search } from "@/components";
 import { FC } from "react";
-import { Link } from "wouter";
 
 export const Navbar: FC = () => (
 	<nav>
 		<Search />
-		{links.map(({ id, text, link }) => (
-			<Link key={id} href={link}>
-				<b>{text}</b>
-			</Link>
-		))}
+		<Links />
 	</nav>
 );
