164c8cf2e51e0837a591bdb9e4238e05aae9727a
Martin <maguera37@gmail.com>
2024-01-20 21:57:58 -0300

    feat: add search component in navbar component

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,9 +1,11 @@
+import { Search } from "@/components";
 import { links } from "@/utils";
 import { FC } from "react";
 import { Link } from "wouter";
 
 export const Navbar: FC = () => (
 	<nav>
+		<Search />
 		{links.map(({ id, text, link }) => (
 			<Link key={id} href={link}>
 				<b>{text}</b>
