4693770adc30781502ae7c5d32cfcac095867b42
Martin <maguera37@gmail.com>
2024-01-20 22:36:41 -0300

    feat: add export of title component

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,3 +3,4 @@ export { Search } from "./search";
 export { Links } from "./links";
 export { BtnsSign } from "./btns-sign";
 export { SwitchColorMode } from "./switch-color-mode";
+export { Title } from "./title";
