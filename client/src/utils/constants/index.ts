33742adfa13ec1397bd966d4321c1375198f2a55
Martin <maguera37@gmail.com>
2024-01-20 21:50:20 -0300

    feat: add links const to constants folder

---
+++ client/src/utils/constants/index.ts
@@ -0,0 +1,17 @@
+export const links = [
+	{
+		id: "home",
+		text: "Home",
+		link: "/",
+	},
+	{
+		id: "about",
+		text: "About",
+		link: "/about",
+	},
+	{
+		id: "projects",
+		text: "Projects",
+		link: "/projects",
+	},
+];
