58677c1ff2bc8fc584c07ad0b7227f01a9e9788d
Martin <maguera37@gmail.com>
2024-01-20 22:58:58 -0300

    feat: add style to search component

--- client/src/components/search/index.tsx
+++ client/src/components/search/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 
 export const Search: FC = () => (
-	<form action="GET">
-		<input type="search" name="search" id="search" />
+	<form action="GET" className="w-auto h-auto">
+		<input
+			type="search"
+			name="search"
+			id="search"
+			className="px-3 py-2 rounded-2xl text-xl text-white bg-red-200"
+			placeholder="Search..."
+		/>
 	</form>
 );
