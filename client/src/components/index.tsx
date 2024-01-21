5e03bb79eec7f7195bff78379620edbef498cfb5
Martin <maguera37@gmail.com>
2024-01-20 22:30:54 -0300

    feat: add export of switch-color-mode component

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,3 +2,4 @@ export { Navbar } from "./navbar";
 export { Search } from "./search";
 export { Links } from "./links";
 export { BtnsSign } from "./btns-sign";
+export { SwitchColorMode } from "./switch-color-mode";
