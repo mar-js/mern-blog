d688de9b8f50e907d69d320268c0bc4c2534522f
Martin <maguera37@gmail.com>
2024-01-20 22:31:51 -0300

    feat: add switch-color-mode component in navbar component

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,10 +1,11 @@
-import { BtnsSign, Links, Search } from "@/components";
+import { BtnsSign, Links, Search, SwitchColorMode } from "@/components";
 import { FC } from "react";
 
 export const Navbar: FC = () => (
 	<nav>
 		<Search />
 		<Links />
+		<SwitchColorMode />
 		<BtnsSign />
 	</nav>
 );
