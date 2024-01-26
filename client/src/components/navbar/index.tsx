d0a6a01fbbc2e178320f08f836db135eb14e2548
Martin <maguera37@gmail.com>
2024-01-26 15:03:00 -0300

    feat: add useDetectMobileView hook and menu component in navbar component

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,13 +1,24 @@
-import { BtnsSign, Links, Search, SwitchColorMode } from "@/components";
+import { BtnsSign, Links, Menu, Search, SwitchColorMode } from "@/components";
+import { useDetectMobileView } from "@/hooks";
 import { FC } from "react";
 
-export const Navbar: FC = () => (
-	<nav className="flex w-full justify-between items-center">
-		<Search />
-		<Links />
-		<div className="flex">
-			<SwitchColorMode />
-			<BtnsSign />
-		</div>
-	</nav>
-);
+export const Navbar: FC = () => {
+	const { detectMobileView } = useDetectMobileView();
+
+	return (
+		<nav className="flex w-full justify-between items-center">
+			<Search />
+			{detectMobileView ? (
+				<Menu />
+			) : (
+				<>
+					<Links />
+					<div className="flex">
+						<SwitchColorMode />
+						<BtnsSign />
+					</div>
+				</>
+			)}
+		</nav>
+	);
+};
