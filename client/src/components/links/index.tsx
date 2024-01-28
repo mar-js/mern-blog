54b4bc1db01a50340cd708c83ea23bfae93f2819
Martin <maguera37@gmail.com>
2024-01-27 23:14:11 -0300

    feat: add style for mobile version to btns-sign component

--- client/src/components/links/index.tsx
+++ client/src/components/links/index.tsx
@@ -3,11 +3,18 @@ import { FC } from "react";
 import { Link } from "wouter";
 
 export const Links: FC = () => (
-	<ul className="flex justify-center items-center text-gray-500">
+	<ul className="flex justify-center items-center text-gray-500 max-[1000px]:w-full max-[1000px]:inline max-[1000px]:text-4xl max-[1000px]:text-center max-[1000px]:text-white">
 		{links.map(({ id, text, link }) => (
-			<li className="mx-2">
-				<Link key={id} href={link} className="hover:opacity-70">
+			<li className="mx-2 max-[1000px]:mb-10 max-[1000px]:text-inherit">
+				<Link
+					href={link}
+					key={id}
+					className="hover:opacity-70 max-[1000px]:flex max-[1000px]:justify-between max-[1000px]:items-center"
+				>
 					{text}
+					<span className="hidden max-[1000px]:inline max-[1000px]:text-yellow-200">
+						{">"}
+					</span>
 				</Link>
 			</li>
 		))}
