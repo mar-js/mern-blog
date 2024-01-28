97db0e9cf186d11eccaebb6f29854333bdb1722d
Martin <maguera37@gmail.com>
2024-01-27 23:14:27 -0300

    feat: add style for mobile version to switch-color-mode component

--- client/src/components/switch-color-mode/index.tsx
+++ client/src/components/switch-color-mode/index.tsx
@@ -13,12 +13,12 @@ export const SwitchColorMode: FC = () => {
 		<button
 			type="button"
 			onClick={handleChangeColorMode}
-			className="border rounded-full p-2 border-blue-500 hover:opacity-70"
+			className="border rounded-full p-2 border-blue-500 hover:opacity-70 w-6 max-[1000px]:mb-20 max-[1000px]:p-0 max-[1000px]:w-24 max-[1000px]:border-none"
 		>
 			{colorMode ? (
-				<img src={moonImg} alt="Moon" className="object-cover w-6" />
+				<img src={moonImg} alt="Moon" className="object-cover w-full" />
 			) : (
-				<img src={sunImg} alt="Sun" className="object-cover w-6" />
+				<img src={sunImg} alt="Sun" className="object-cover w-full" />
 			)}
 		</button>
 	);
