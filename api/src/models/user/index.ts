b98c158b02b7683e0f652332790ea0d87f853e81
Martin <maguera37@gmail.com>
2024-01-30 22:06:44 -0300

    feat: create user model

---
+++ api/src/models/user/index.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userChema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			min: 3,
+			max: 30,
+			unique: true,
+		},
+		email: {
+			type: String,
+			requerid: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+export const UserModel = model("User", userChema);
