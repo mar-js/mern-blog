0e802e9eba27b7be2e166cd15b3cc5638c8edfbe
Martin <maguera37@gmail.com>
2024-01-30 21:52:49 -0300

    feat: change uri to environment

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 
-mongoose.connect(
-	"mongodb+srv://mar:mar@mern-blog.fsgs44o.mongodb.net/mern-blog?retryWrites=true&w=majority",
-);
+mongoose.connect(process.env.MONGO_DB_URI);
 
 const APP = express();
 
