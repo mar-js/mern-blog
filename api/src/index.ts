b51f8968d16a9d5ae50339d25a20060f8b1299e1
Martin <maguera37@gmail.com>
2024-01-31 22:35:08 -0300

    feat: use usersRoute and authRoute in src/index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,16 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
+import { authRoute, usersRoute } from "./routes";
 
 mongoose.connect(process.env.MONGO_DB_URI);
 
 const APP = express();
 
 APP.use(json());
+
+APP.use("/api", usersRoute);
+
+APP.use("/api", authRoute);
 
 APP.listen(3000, () => {
 	console.log("run");
