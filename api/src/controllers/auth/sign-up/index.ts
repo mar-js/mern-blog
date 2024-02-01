20cc5386650000a4f4148c9a4ee187da0c3d40ca
Martin <maguera37@gmail.com>
2024-01-31 23:34:51 -0300

    feat: add logic in signUpController

--- api/src/controllers/auth/sign-up/index.ts
+++ api/src/controllers/auth/sign-up/index.ts
@@ -1,5 +1,35 @@
+import { hashSync } from "bcrypt";
 import { Request, Response } from "express";
+import { UserModel } from "../../../models";
+
+interface InterfaceReqBody {
+	username: string;
+	email: string;
+	password: string;
+}
 
 export const signUpController = async (req: Request, res: Response) => {
-	console.log(req.body);
+	if (typeof req.body === "undefined")
+		return res
+			.status(400)
+			.json({ message: "You must complete the required information" });
+
+	const { email, password, username } = req.body as InterfaceReqBody;
+
+	if (!email?.length || !password?.length || !username?.length)
+		return res.status(400).json({ message: "All fields are required" });
+
+	const newUser = new UserModel({
+		email,
+		password: hashSync(password, 10),
+		username,
+	});
+
+	try {
+		await newUser.save();
+
+		return res.status(200).json({ message: "Signup successful" });
+	} catch (error) {
+		return res.status(500).json({ message: error?.message });
+	}
 };
