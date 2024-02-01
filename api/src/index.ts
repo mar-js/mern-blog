f5c9b88687350cafb960ae1dd8daa96691b31791
Martin <maguera37@gmail.com>
2024-01-31 22:34:31 -0300

    feat: add APP.use(json()) in src/index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 
 mongoose.connect(process.env.MONGO_DB_URI);
 
 const APP = express();
 
-APP.use("/", (req, res) => {
-	res.send("<b>HOLAAA</b>");
-});
+APP.use(json());
 
 APP.listen(3000, () => {
 	console.log("run");
