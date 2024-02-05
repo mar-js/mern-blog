5be3ddd76af296fefd1c5f2abd9219349157a5ba
Martin <maguera37@gmail.com>
2024-02-05 10:03:00 -0300

    feat: add import of section component in error page

--- client/src/pages/error/index.tsx
+++ client/src/pages/error/index.tsx
@@ -1,3 +1,4 @@
+import { Section } from "@/layouts";
 import { FC } from "react";
 
-export const ErrorPage: FC = () => <section>Error Page</section>;
+export const ErrorPage: FC = () => <Section>Error Page</Section>;
