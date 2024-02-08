5e7d80c298b13ac68c919efac48bb108169affb5
Martin <maguera37@gmail.com>
2024-02-08 08:48:10 -0300

    feat: change size of switch-color-mode component

--- client/src/components/switch-color-mode/index.tsx
+++ client/src/components/switch-color-mode/index.tsx
@@ -13,7 +13,7 @@ export const SwitchColorMode: FC = () => {
 		<button
 			type="button"
 			onClick={handleChangeColorMode}
-			className="border rounded-full p-2 border-blue-500 hover:opacity-70 w-6 max-[1000px]:mb-20 max-[1000px]:p-0 max-[1000px]:w-24 max-[1000px]:border-none"
+			className="border rounded-full p-2 border-blue-500 hover:opacity-70 w-11 max-[1000px]:mb-20 max-[1000px]:p-0 max-[1000px]:w-24 max-[1000px]:border-none"
 		>
 			{colorMode ? (
 				<img src={moonImg} alt="Moon" className="object-cover w-full" />
