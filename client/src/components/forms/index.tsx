2393335cbf16452e53f07650c0b8d8d99bb8efaa
Martin <maguera37@gmail.com>
2024-02-10 16:54:59 -0300

    feat: create handleSubmit for sign-up in forms component

--- client/src/components/forms/index.tsx
+++ client/src/components/forms/index.tsx
@@ -1,9 +1,31 @@
-import { FC } from "react";
+import { FC, FormEvent } from "react";
 import { SignUp } from "./sign-up";
 
 export const Forms: FC = () => {
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+
+		const TARGET = e.target as HTMLFormElement;
+		const DATA = Object.fromEntries(new FormData(TARGET));
+
+		try {
+			const RESPONSE = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(DATA),
+			});
+			const RESPONSE_DATA = await RESPONSE.json();
+
+			console.log(RESPONSE_DATA);
+		} catch (error) {
+			console.log("A", error);
+		}
+	};
+
 	return (
-		<form action="POST" className="w-[35rem]">
+		<form action="POST" onSubmit={handleSubmit} className="w-[35rem]">
 			<SignUp />
 		</form>
 	);
