06f873ea3a425913ff3baedc916e949dde0294f2
Martin <maguera37@gmail.com>
2024-02-10 17:11:11 -0300

    feat: add style and components in footer layout

--- client/src/layouts/footer/index.tsx
+++ client/src/layouts/footer/index.tsx
@@ -1,3 +1,9 @@
+import { Copyright, Title } from "@/components";
 import { FC } from "react";
 
-export const Footer: FC = () => <footer>Footer</footer>;
+export const Footer: FC = () => (
+	<footer className="border-t-8 border-blue-200 p-8 flex justify-between items-center">
+		<Title />
+		<Copyright />
+	</footer>
+);
