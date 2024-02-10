4ce370cec985d73072a180ce5d14146cd5ff8917
Martin <maguera37@gmail.com>
2024-02-10 16:12:43 -0300

    feat: add components in sign-up page

--- client/src/pages/sign-up/index.tsx
+++ client/src/pages/sign-up/index.tsx
@@ -1,4 +1,16 @@
+import { Forms, Title } from "@/components";
 import { Section } from "@/layouts";
 import { FC } from "react";
 
-export const SignUpPage: FC = () => <Section>SignUp Page</Section>;
+export const SignUpPage: FC = () => (
+	<Section className="flex flex-wrap px-10 h-[30rem] items-center justify-evenly">
+		<div className="flex flex-col">
+			<Title />
+			<p className="text-sm italic mt-4">
+				This is demo project. You can sign up with your email and password or
+				with Google
+			</p>
+		</div>
+		<Forms />
+	</Section>
+);
