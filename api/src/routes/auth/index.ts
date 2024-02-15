731c11d2bdb15cd4757e53073d7e7436666a4585
Martin <maguera37@gmail.com>
2024-02-15 18:10:01 -0300

    feat: add export signInRoute in auth route

--- api/src/routes/auth/index.ts
+++ api/src/routes/auth/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { signInRoute } from "./sign-in";
 import { signUpRoute } from "./sign-up";
 
 export const authRoute = Router();
 
 authRoute.use("/auth", signUpRoute);
+authRoute.use("/auth", signInRoute);
