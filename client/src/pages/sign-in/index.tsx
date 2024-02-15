b40ae67c2874c909926fbba14ff32c1d22860f26
Martin <maguera37@gmail.com>
2024-02-15 18:11:47 -0300

    feat: add components in sign-in page

--- client/src/pages/sign-in/index.tsx
+++ client/src/pages/sign-in/index.tsx
@@ -1,4 +1,16 @@
+import { Forms, Title } from "@/components";
 import { Section } from "@/layouts";
 import { FC } from "react";
 
-export const SignInPage: FC = () => <Section>SignIn Page</Section>;
+export const SignInPage: FC = () => (
+	<Section className="flex flex-wrap px-10 h-[30rem] items-center justify-evenly">
+		<div className="flex flex-col">
+			<Title />
+			<p className="text-sm italic mt-4">
+				This is demo project. You can sign in with your email and password or
+				with Google
+			</p>
+		</div>
+		<Forms />
+	</Section>
+);
