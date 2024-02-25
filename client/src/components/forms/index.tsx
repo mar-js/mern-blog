89ca93d0344d02b25777478c996f3844aa2acd0d
Martin <maguera37@gmail.com>
2024-02-24 23:27:19 -0300

    feat: add useUsersContext and dispatchs

--- client/src/components/forms/index.tsx
+++ client/src/components/forms/index.tsx
@@ -1,3 +1,9 @@
+import { useUsersContext } from "@/contexts";
+import {
+	userErrorAction,
+	userLoadingAction,
+	userSuccessAction,
+} from "@/helpers";
 import { FC, FormEvent } from "react";
 import { navigate } from "wouter/use-location";
 import { Access } from "./access";
@@ -7,11 +13,15 @@ interface IForms {
 }
 
 export const Forms: FC<IForms> = ({ isSignUp }) => {
+	const { userDispatch } = useUsersContext();
+
 	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault();
 
 		const TARGET = e.target as HTMLFormElement;
 		const DATA = Object.fromEntries(new FormData(TARGET));
+
+		userDispatch(userLoadingAction());
 
 		const RESPONSE = await fetch(
 			`/api/auth/${isSignUp ? "signup" : "signin"}`,
@@ -23,14 +33,20 @@ export const Forms: FC<IForms> = ({ isSignUp }) => {
 				body: JSON.stringify(DATA),
 			},
 		);
+
 		const RESPONSE_DATA = await RESPONSE.json();
 
 		if (RESPONSE.ok) {
+			userDispatch(userSuccessAction(RESPONSE_DATA));
+
 			return navigate(isSignUp ? "/sign-in" : "/");
 		}
 
+		userDispatch(userErrorAction(RESPONSE_DATA));
+
 		return navigate("/error");
 	};
+
 	return (
 		<form action="POST" onSubmit={handleSubmit} className="w-[35rem]">
 			<Access isSignUp={isSignUp} />
