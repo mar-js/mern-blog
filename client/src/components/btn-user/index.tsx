6d96e1820aa94cbb26d54f5bbab6a1ae49426831
Martin <maguera37@gmail.com>
2024-02-26 22:58:08 -0300

    feat: add dropdown component to btn-user component

--- client/src/components/btn-user/index.tsx
+++ client/src/components/btn-user/index.tsx
@@ -1,8 +1,51 @@
 import user from "@/assets/user.svg";
-import { FC } from "react";
+import { useUsersContext } from "@/contexts";
+import { FC, useState } from "react";
+import { Link } from "wouter";
 
-export const BtnUser: FC = () => (
-	<button type="button" className="object-cover ml-3 w-11 hover:opacity-70">
-		<img src={user} alt="User icon" className="object-cover w-full" />
-	</button>
-);
+export const BtnUser: FC = () => {
+	const [showDropdown, setShowDropdown] = useState(false);
+	const {
+		userState: { data },
+	} = useUsersContext();
+	const { username, email } = data.user;
+
+	const handleClick = () => {
+		setShowDropdown((prev) => !prev);
+	};
+
+	return (
+		<>
+			<button
+				type="button"
+				className="object-cover ml-3 w-11 hover:opacity-70"
+				onClick={handleClick}
+			>
+				<img src={user} alt="User icon" className="object-cover w-full" />
+			</button>
+			{showDropdown && (
+				<div className="object-cover absolute top-12 right-0 border-2 bg-white">
+					<div className="border-b-2 px-2 py-4">
+						<h6>
+							{username} {email}
+						</h6>
+					</div>
+					<div className="flex flex-col">
+						<Link
+							href="/dashboard?tab=profile"
+							className="p-2 hover:text-white hover:bg-blue-500"
+						>
+							Profile
+						</Link>
+						<button
+							type="button"
+							className="text-left p-2 hover:text-white hover:bg-blue-500"
+						>
+							Sign out
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
