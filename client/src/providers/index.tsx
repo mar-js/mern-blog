bce23f6413359287d156bb32e2caad3173e77367
Martin <maguera37@gmail.com>
2024-02-26 23:16:40 -0300

    feat: create handle providers component

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,2 +1,9 @@
-export { UsersProvider } from "./users";
-export { ThemeProvider } from "./theme";
+import { FC, PropsWithChildren } from "react";
+import { ThemeProvider } from "./theme";
+import { UsersProvider } from "./users";
+
+export const Providers: FC<PropsWithChildren> = ({ children }) => (
+	<ThemeProvider>
+		<UsersProvider>{children}</UsersProvider>
+	</ThemeProvider>
+);
