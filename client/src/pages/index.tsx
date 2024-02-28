b7affebd0de743999f9cbbf47ad193727133ce6f
Martin <maguera37@gmail.com>
2024-02-27 23:12:09 -0300

    feat: add isLogged const of user context and config privacity of router in pages component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { useUsersContext } from "@/contexts";
 import { Footer, Header } from "@/layouts";
 import { FC } from "react";
-import { Route, Switch } from "wouter";
+import { Redirect, Route, Switch } from "wouter";
 import { AboutPage } from "./about";
 import { DashboardPage } from "./dashboard";
 import { ErrorPage } from "./error";
@@ -9,18 +10,26 @@ import { ProjectsPage } from "./projects";
 import { SignInPage } from "./sign-in";
 import { SignUpPage } from "./sign-up";
 
-export const Pages: FC = () => (
-	<>
-		<Header />
-		<Switch>
-			<Route path="/about" component={AboutPage} />
-			<Route path="/dashboard" component={DashboardPage} />
-			<Route path="*" component={ErrorPage} />
-			<Route path="/" component={HomePage} />
-			<Route path="/projects" component={ProjectsPage} />
-			<Route path="/sign-in" component={SignInPage} />
-			<Route path="/sign-up" component={SignUpPage} />
-		</Switch>
-		<Footer />
-	</>
-);
+export const Pages: FC = () => {
+	const { isLogged } = useUsersContext();
+
+	return (
+		<>
+			<Header />
+			<Switch>
+				<Route path="/about" component={AboutPage} />
+				{isLogged ? (
+					<Route path="/dashboard" component={DashboardPage} />
+				) : (
+					<Redirect to="/" />
+				)}
+				<Route path="*" component={ErrorPage} />
+				<Route path="/" component={HomePage} />
+				<Route path="/projects" component={ProjectsPage} />
+				<Route path="/sign-in" component={SignInPage} />
+				<Route path="/sign-up" component={SignUpPage} />
+			</Switch>
+			<Footer />
+		</>
+	);
+};
