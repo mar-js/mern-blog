c5c1a2dfd23eac5ca33ca1d68f7751c49c92bbac
Martin <maguera37@gmail.com>
2024-02-27 22:54:50 -0300

    feat: add context of theme in switch-color-mode component

--- client/src/components/switch-color-mode/index.tsx
+++ client/src/components/switch-color-mode/index.tsx
@@ -1,14 +1,15 @@
 import moonImg from "@/assets/moon.png";
 import sunImg from "@/assets/sun.png";
 import { useThemeContext } from "@/contexts";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 export const SwitchColorMode: FC = () => {
-	const [colorMode, setColorMode] = useState(false);
-	const { handleToggleThemeMode } = useThemeContext();
+	const {
+		handleToggleThemeMode,
+		themeMode: { showDarkMode },
+	} = useThemeContext();
 
 	const handleChangeColorMode = () => {
-		setColorMode((prev) => !prev);
 		handleToggleThemeMode();
 	};
 
@@ -18,7 +19,7 @@ export const SwitchColorMode: FC = () => {
 			onClick={handleChangeColorMode}
 			className="border-2 rounded-full p-2 border-blue-500 hover:opacity-70 w-11 max-[1000px]:mb-20 max-[1000px]:p-0 max-[1000px]:w-24 max-[1000px]:border-none"
 		>
-			{colorMode ? (
+			{showDarkMode ? (
 				<img src={moonImg} alt="Moon" className="object-cover w-full" />
 			) : (
 				<img src={sunImg} alt="Sun" className="object-cover w-full" />
