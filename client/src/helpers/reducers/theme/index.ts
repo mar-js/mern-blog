90c00f9717e4f21c1a5a87d61c68898cdff341a8
Martin <maguera37@gmail.com>
2024-02-27 22:53:51 -0300

    feat: create theme reducer in helpers/reducers

---
+++ client/src/helpers/reducers/theme/index.ts
@@ -0,0 +1,50 @@
+const THEME_MODE_CHANGE = "THEME_MODE_CHANGE";
+const THEME_MODE_CHANGE_ICON = "THEME_MODE_CHANGE_ICON";
+
+export const themeModeReducer = (state, action) => {
+	const { type, payload } = action;
+
+	if (type === THEME_MODE_CHANGE_ICON)
+		return {
+			...state,
+			showDarkMode: payload,
+		};
+
+	if (type === THEME_MODE_CHANGE)
+		return {
+			...state,
+			colorMode: payload,
+		};
+
+	return { ...state };
+};
+
+export const themeModeChangeIconAction = (theme) => ({
+	type: THEME_MODE_CHANGE_ICON,
+	payload: theme,
+});
+
+export const themeModeChangeAction = (theme) => ({
+	type: THEME_MODE_CHANGE,
+	payload: theme,
+});
+
+/*
+export const signInParse = (initialState) => {
+	const user = localStorage.getItem("user");
+
+	if (user) {
+		return parseJson(user);
+	}
+
+	return initialState;
+};
+
+function parseJson(json) {
+	try {
+		return JSON.parse(json);
+	} catch (error) {
+		console.log(error);
+	}
+}
+*/
