c48384c94cca245e1eb7d6da2db493982a778948
Martin <maguera37@gmail.com>
2024-03-02 00:11:43 -0300

    feat: add isLogged condition to dashboard page

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,4 +1,12 @@
+import { useUsersContext } from "@/contexts";
 import { Section } from "@/layouts";
 import { FC } from "react";
+import { navigate } from "wouter/use-location";
 
-export const DashboardPage: FC = () => <Section>Dashboard Page</Section>;
+export const DashboardPage: FC = () => {
+	const { userState } = useUsersContext();
+
+	if (!userState.isLogged) navigate("/");
+
+	return <Section>Dashboard Page</Section>;
+};
