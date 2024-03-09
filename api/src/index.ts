b1e8114db6660f2ba0de7086ee219c7ab0254760
Martin <maguera37@gmail.com>
2024-03-09 19:06:35 -0300

    feat: remove config of errors middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
-import { errorsMiddleware } from "./middlewares";
 import { authRoute, usersRoute } from "./routes";
 
 mongoose.connect(process.env.MONGO_DB_URI);
@@ -12,14 +11,6 @@ APP.use(json());
 APP.use("/api", usersRoute);
 
 APP.use("/api", authRoute);
-
-const errorHandlerMiddleware = (err, req, res, next) => {
-	// Handle the error
-	console.log("Executing error handling middleware");
-	res.status(500).json({ error: "Internal Server Error" });
-};
-
-APP.use(errorHandlerMiddleware);
 
 APP.listen(3000, () => {
 	console.log("run");
