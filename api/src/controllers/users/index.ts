852a7c79a2ccfa5ff42d1618f2abd499dd84d0a4
Martin <maguera37@gmail.com>
2024-03-09 19:08:58 -0300

    feat: add export of update controller to controller/users file

--- api/src/controllers/users/index.ts
+++ api/src/controllers/users/index.ts
@@ -1,5 +1,1 @@
-import { Request, Response } from "express";
-
-export const usersController = (req: Request, res: Response) => {
-	res.send("<h2>USERS</h2>");
-};
+export { userUpdateController } from "./update";
