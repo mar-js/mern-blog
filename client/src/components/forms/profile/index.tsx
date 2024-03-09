b574eb9eba19d9c30904c6302b57c1eb00a55d35
Martin <maguera37@gmail.com>
2024-03-09 20:28:35 -0300

    feat: add name attr to the inputs elements in profile component

--- client/src/components/forms/profile/index.tsx
+++ client/src/components/forms/profile/index.tsx
@@ -1,33 +1,31 @@
+import { useUsersContext } from "@/contexts";
 import { FC } from "react";
 
 export const Profile: FC = () => {
+	const { userState } = useUsersContext();
+	const { username, email } = userState?.data?.user;
+
 	return (
 		<div className="w-full flex justify-center items-center flex-col">
 			<h3 className="text-3xl mb-10">Profile</h3>
 			<div className="flex flex-col w-full mb-10">
 				<input
 					className="mb-6 outline outline-1 outline-gray-300 focus-within:shadow-md focus-within:shadow-gray-400 focus-within:outline-none p-2"
-					placeholder="a"
+					name="username"
+					placeholder={username}
 					type="text"
-					name=""
-					id=""
-					required
 				/>
 				<input
 					className="mb-6 outline outline-1 outline-gray-300 focus-within:shadow-md focus-within:shadow-gray-400 focus-within:outline-none p-2"
-					placeholder="b"
+					name="email"
+					placeholder={email}
 					type="text"
-					name=""
-					id=""
-					required
 				/>
 				<input
 					className="mb-6 outline outline-1 outline-gray-300 focus-within:shadow-md focus-within:shadow-gray-400 focus-within:outline-none p-2"
-					placeholder="c"
+					placeholder="******"
+					name="password"
 					type="text"
-					name=""
-					id=""
-					required
 				/>
 				<button
 					className="border p-2 hover:opacity-70 hover:border-blue-500"
