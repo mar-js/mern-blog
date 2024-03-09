13feb89ae9c0e505aa774f59fd4b096337e81451
Martin <maguera37@gmail.com>
2024-03-09 20:27:41 -0300

    feat: add verifyToken to users/update route

--- api/src/routes/users/update/index.ts
+++ api/src/routes/users/update/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userUpdateController } from "../../../controllers";
+import { verifyToken } from "../../../utils";
 
 export const userUpdateRoute = Router();
 
-userUpdateRoute.put("/update/:userId", userUpdateController);
+userUpdateRoute.put("/update/:userId", verifyToken, userUpdateController);
