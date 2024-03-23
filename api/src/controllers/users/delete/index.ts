0b5e742412e894cfa3638639f5a2c176918958be
Martin <maguera37@gmail.com>
2024-03-22 22:38:00 -0300

    feat: create controller of delete user

---
+++ api/src/controllers/users/delete/index.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from "express";
+import { UserModel } from "../../../models";
+import { errorsHandler } from "../../../utils";
+
+export const userDeleteController = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	if (req.user.id !== req.params.userId)
+		return next(
+			errorsHandler({
+				statusCode: 403,
+				message: "You are not allowed to delete this user",
+			}),
+		);
+
+	try {
+		await UserModel.findByIdAndDelete(req.params.userId);
+
+		return res.status(200).json({ user: null, message: "Update successful" });
+	} catch (error) {
+		next(error);
+	}
+};
--- api/src/controllers/users/index.ts
+++ api/src/controllers/users/index.ts
@@ -1,1 +1,2 @@
 export { userUpdateController } from "./update";
+export { userDeleteController } from "./delete";
