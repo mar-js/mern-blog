bef1ed9ae537b6cd67bc6cd987366df47582a9f6
Martin <maguera37@gmail.com>
2024-03-27 22:38:00 -0300

    feat: move handleSubmit to helpers/handlers

--- client/src/providers/users/index.tsx
+++ client/src/providers/users/index.tsx
@@ -1,14 +1,7 @@
 import { UsersModel } from "@/contexts";
-import type { IUserState, IUsersModel } from "@/contexts/users/interface";
-import {
-	userErrorAction,
-	userLoadingAction,
-	userReducer,
-	userSuccessAction,
-} from "@/helpers";
-import type { FC, FormEvent, PropsWithChildren } from "react";
-import { useEffect, useReducer } from "react";
-import { navigate } from "wouter/use-location";
+import { handleSubmitUser, userReducer, userSuccessAction } from "@/helpers";
+import type { FC, PropsWithChildren } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 
 export const UsersProvider: FC<PropsWithChildren> = ({ children }) => {
 	const initialState: IUserState = {
@@ -22,54 +15,21 @@ export const UsersProvider: FC<PropsWithChildren> = ({ children }) => {
 	const USER = USER_STORAGE && JSON.parse(USER_STORAGE);
 	const ID = USER?.id || userState.data?.id || "";
 
-	const handleSubmit = async (e: FormEvent, access: string) => {
-		e.preventDefault();
-
-		const TARGET = e.target as HTMLFormElement;
-		const GET_DATA = Object.fromEntries(new FormData(TARGET));
-		const DATA =
-			access === "update"
-				? Object.fromEntries(
-						Object.entries(GET_DATA).filter(([_, v]) => v !== ""),
-					)
-				: GET_DATA;
-		const ENDPOINT =
-			ID && access === "update" ? `users/update/${ID}` : `auth/${access}`;
-
-		userDispatch(userLoadingAction());
-
-		try {
-			const RESPONSE = await fetch(`/api/${ENDPOINT}`, {
-				method: access === "update" ? "PUT" : "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(DATA),
-			});
-
-			const { user } = await RESPONSE.json();
-
-			if (RESPONSE.ok) {
-				userDispatch(userSuccessAction({ ...user }));
-
-				return navigate(access === "signup" ? "/signin" : "/");
-			}
-
-			throw "ERROR CLIENT";
-		} catch (error) {
-			userDispatch(userErrorAction({ error: error as string }));
-			userDispatch(userSuccessAction(null));
-
-			return navigate("/error");
-		}
-	};
+	const handleSubmitUserMemorize = useCallback(
+		(access, e) => handleSubmitUser({ access, e, id, userDispatch }),
+		[],
+	);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
 		if (USER) userDispatch(userSuccessAction(USER));
 	}, []);
 
-	const VALUE: IUsersModel = { userState, userDispatch, handleSubmit };
+	const VALUE: IUsersModel = {
+		userState,
+		userDispatch,
+		handleSubmitUser: handleSubmitUserMemorize,
+	};
 
 	return <UsersModel.Provider value={VALUE}>{children}</UsersModel.Provider>;
 };
