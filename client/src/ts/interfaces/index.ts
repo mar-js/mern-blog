2a448d420cdd5b20c5ad5cfd1931feafd08e83bd
Martin <maguera37@gmail.com>
2024-03-29 22:38:00 -0300

    feat: change any to Partial<IUser | boolean | { error: string }> in type of payload of IAction

--- client/src/ts/interfaces/index.ts
+++ client/src/ts/interfaces/index.ts
@@ -18,7 +18,7 @@ export interface IUserState {
 
 export interface IAction {
 	type: string;
-	payload: any;
+	payload: Partial<IUser | boolean | { error: string }>;
 }
 
 export interface IHandleSubmitUser {
