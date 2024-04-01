668c4ce30428ecd25d2194738b8cba6a8fa0da53
Martin <maguera37@gmail.com>
2024-03-31 22:38:00 -0300

    feat: create ColorMode type

--- client/src/ts/types/index.ts
+++ client/src/ts/types/index.ts
@@ -8,8 +8,15 @@ const M = {
 const A = {
 	Signin: "signin",
 	Signup: "signup",
-	...M,
+} as const;
+
+const Cm = {
+	Dark: "dark",
+	Light: "light",
 } as const;
 
 export type Method = typeof M;
+
 export type Access = typeof A;
+
+export type ColorMode = typeof Cm;
