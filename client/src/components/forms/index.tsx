94f889ca1a423b079c9d241ff03ca61406c8437e
Martin <maguera37@gmail.com>
2024-04-24 23:38:00 -0300

    feat: add conditional in action and handlerSubmitUser function in forms component

--- client/src/components/forms/index.tsx
+++ client/src/components/forms/index.tsx
@@ -1,16 +1,17 @@
 import { useUsersContext } from "@/contexts";
-import { IAccess } from "@/ts/interfaces";
+import { handlerSubmitUser } from "@/helpers";
+import type { IAccess } from "@/ts/interfaces";
 import type { FC } from "react";
 import { Access } from "./access";
 import { Profile } from "./profile";
 
 export const Forms: FC<IAccess> = ({ access }) => {
-	const { handleSubmit } = useUsersContext();
+	const { userDispatch, id } = useUsersContext();
 
 	return (
 		<form
-			action="POST"
-			onSubmit={(e) => handleSubmit(e, access)}
+			action={access === "update" ? "PUT" : "POST"}
+			onSubmit={(e) => handlerSubmitUser({ e, access, userDispatch, id })}
 			className={
 				access === "update" ? "w-3/4 max-[1000px]:w-full" : "w-[35rem]"
 			}
