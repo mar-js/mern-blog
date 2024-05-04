49cc764d9e2bb2315b94b7e008b2b124b644d9ba
Martin <maguera37@gmail.com>
2024-05-03 23:38:00 -0300

    feat: create BtnAction component

---
+++ client/src/components/btns/action/index.tsx
@@ -0,0 +1,23 @@
+import type { IBtnAction } from "@/ts/interfaces";
+import type { FC } from "react";
+
+export const BtnAction: FC<IBtnAction> = ({ access, className }) => {
+	const text =
+		access === "signin"
+			? "Sign In"
+			: access === "signup"
+				? "Sign Up"
+				: access === "delete"
+					? "Delete Account"
+					: "Update Account";
+
+	return (
+		<button
+			id={access}
+			className={`w-auto py-2 px-4 ${className}`}
+			type="submit"
+		>
+			{text}
+		</button>
+	);
+};
