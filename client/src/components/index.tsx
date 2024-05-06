7b10678606e0a6ab926a4dd815b7e84efc5a13b1
Martin <maguera37@gmail.com>
2024-05-05 23:38:00 -0300

    feat: add export of components

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,10 +1,9 @@
 export { Navbar } from "./navbar";
 export { Search } from "./search";
 export { Links } from "./links";
-export { BtnsSign } from "./btns-sign";
 export { SwitchColorMode } from "./switch-color-mode";
 export { Title } from "./title";
 export { Menu } from "./menu";
-export { Forms } from "./forms";
+export * from "./forms";
 export { Copyright } from "./copyright";
-export { BtnUser } from "./btn-user";
+export * from "./btns";
