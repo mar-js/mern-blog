44a6deed8abc39e4c3ea8aa1222875cbcbce3822
Martin <maguera37@gmail.com>
2024-05-20 23:38:00 -0300

    feat: add condition for signout to createMethod and createEndpoint function

--- client/src/helpers/creators/endpoint/index.ts
+++ client/src/helpers/creators/endpoint/index.ts
@@ -3,7 +3,7 @@ import type { Access } from "@/ts/types";
 export const createEndpoint = (access: Access, id: string): string => {
 	let endpoint = "";
 
-	if (access === "signin" || access === "signup") {
+	if (access === "signin" || access === "signup" || access === "signout") {
 		endpoint = `auth/${access}`;
 	} else {
 		endpoint = `users/${access}/${id}`;
--- client/src/helpers/creators/method/index.ts
+++ client/src/helpers/creators/method/index.ts
@@ -3,7 +3,7 @@ import type { Access, Method } from "@/ts/types";
 export const createMethod = (access: Access): Method => {
 	let method = "" as Method;
 
-	if (access === "signin" || access === "signup") {
+	if (access === "signin" || access === "signup" || access === "signout") {
 		method = "POST";
 	} else if (access === "update") {
 		method = "PUT";
