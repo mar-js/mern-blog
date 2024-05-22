0334ecd8286974984f07d9fac2a0972fef01fa5c
Martin <maguera37@gmail.com>
2024-05-21 21:26:46 -0300

    feat: add profile value to Access type

--- client/src/ts/types/index.ts
+++ client/src/ts/types/index.ts
@@ -1,5 +1,11 @@
 export type Method = "GET" | "PUT" | "POST" | "DELETE";
 
-export type Access = "signin" | "signup" | "signout" | "update" | "delete";
+export type Access =
+	| "profile"
+	| "signin"
+	| "signup"
+	| "signout"
+	| "update"
+	| "delete";
 
 export type ColorMode = "dark" | "light";
