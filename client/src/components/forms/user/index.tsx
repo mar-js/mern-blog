a951abd10d866f978e4c8e115e46cd3797acb42b
Martin <maguera37@gmail.com>
2024-05-24 23:46:54 -0300

    feat: add create-post link in User component

--- client/src/components/forms/user/index.tsx
+++ client/src/components/forms/user/index.tsx
@@ -10,9 +10,15 @@ export const User: FC = () => (
 		>
 			Profile
 		</Link>
+		<Link
+			href="/create-post"
+			className="py-2 px-4 hover:text-white hover:bg-cyan-500"
+		>
+			Create post
+		</Link>
 		<BtnAction
 			access="signout"
-			className="text-left hover:text-white hover:bg-blue-500"
+			className="text-left hover:text-white hover:bg-red-500"
 		/>
 	</div>
 );
