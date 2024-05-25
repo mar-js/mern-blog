c8c452cd12575505dc33414d4aa1415ec6b51a5b
Martin <maguera37@gmail.com>
2024-05-24 23:45:31 -0300

    feat: add export of create-post component in forms file

--- client/src/components/forms/index.tsx
+++ client/src/components/forms/index.tsx
@@ -2,3 +2,4 @@ export { Access } from "./access";
 export { Forms } from "./container";
 export { Profile } from "./profile";
 export { User } from "./user";
+export { CreatePost } from "./create-post";
