eebab2eee5086058f85d1503d4445515b7674127
Martin <maguera37@gmail.com>
2024-05-27 22:38:00 -0300

    feat: add introduction-sign component to sign-in page

--- client/src/pages/sign-in/index.tsx
+++ client/src/pages/sign-in/index.tsx
@@ -1,16 +1,10 @@
-import { Forms, Title } from "@/components";
+import { Forms, IntroductionSign } from "@/components";
 import { Section } from "@/layouts";
 import type { FC } from "react";
 
 export const SignInPage: FC = () => (
 	<Section className="flex flex-wrap h-[30rem] items-start justify-evenly">
-		<div className="flex flex-col">
-			<Title />
-			<p className="text-sm italic mt-4">
-				This is demo project. You can sign in with your email and password or
-				with Google
-			</p>
-		</div>
+		<IntroductionSign sign="In" />
 		<Forms access="signin" />
 	</Section>
 );
