1b5725451d69aa1b93e4434e349afa6db0ea5c78
Martin <maguera37@gmail.com>
2024-05-27 22:38:00 -0300

    feat: add introduction-sign component to sign-up page

--- client/src/pages/sign-up/index.tsx
+++ client/src/pages/sign-up/index.tsx
@@ -1,16 +1,10 @@
-import { Forms, Title } from "@/components";
+import { Forms, IntroductionSign } from "@/components";
 import { Section } from "@/layouts";
 import type { FC } from "react";
 
 export const SignUpPage: FC = () => (
 	<Section className="flex flex-wrap h-[30rem] items-start justify-evenly">
-		<div className="flex flex-col">
-			<Title />
-			<p className="text-sm italic mt-4">
-				This is demo project. You can sign up with your email and password or
-				with Google
-			</p>
-		</div>
+		<IntroductionSign sign="Up" />
 		<Forms access="signup" />
 	</Section>
 );
