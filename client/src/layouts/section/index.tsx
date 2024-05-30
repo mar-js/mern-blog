0dc480cdcbe49108922e9410813b96d50558f454
Martin <maguera37@gmail.com>
2024-05-29 21:19:50 -0300

    fix: add condition in prop of className for remove to undefined class in Section layout

--- client/src/layouts/section/index.tsx
+++ client/src/layouts/section/index.tsx
@@ -2,5 +2,7 @@ import type { ILayout } from "@/ts/interfaces";
 import type { FC } from "react";
 
 export const Section: FC<ILayout> = ({ children, className }) => (
-	<section className={`px-10 py-40 h-screen ${className}`}>{children}</section>
+	<section className={`px-10 py-40 h-screen ${className || ""}`}>
+		{children}
+	</section>
 );
