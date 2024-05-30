2ec25b9e3c079d933012502b28f6539dfe1b9400
Martin <maguera37@gmail.com>
2024-05-29 22:04:57 -0300

    feat: add subtitle component to create-post page

--- client/src/pages/create-post/index.tsx
+++ client/src/pages/create-post/index.tsx
@@ -1,9 +1,10 @@
-import { Forms } from "@/components";
+import { Forms, Subtitle } from "@/components";
 import { Section } from "@/layouts";
 import type { FC } from "react";
 
 export const CreatePostPage: FC = () => (
 	<Section>
+		<Subtitle text="Create post" />
 		<Forms access="create-post" isWidthFull />
 	</Section>
 );
