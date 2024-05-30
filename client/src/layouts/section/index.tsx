9a37c9b7a7983657beed6cf0959d4cf2809ba383
Martin <maguera37@gmail.com>
2024-05-29 22:04:29 -0300

    feat: change padding in Section layout

--- client/src/layouts/section/index.tsx
+++ client/src/layouts/section/index.tsx
@@ -2,7 +2,7 @@ import type { ILayout } from "@/ts/interfaces";
 import type { FC } from "react";
 
 export const Section: FC<ILayout> = ({ children, className }) => (
-	<section className={`px-10 py-40 h-screen ${className || ""}`}>
+	<section className={`px-10 py-20 h-screen ${className || ""}`}>
 		{children}
 	</section>
 );
