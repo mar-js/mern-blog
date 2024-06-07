9fc9914044174f112f88eda8da690f1cfdcca0e7
Martin <maguera37@gmail.com>
2024-06-07 18:49:56 -0300

    feat: create links components of dashboard and navbar component

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,6 @@
 export { Navbar } from "./navbar";
 export { Search } from "./search";
-export { Links } from "./links";
+export * from "./links";
 export { SwitchColorMode } from "./switch-color-mode";
 export { Title } from "./title";
 export { Menu } from "./menu";
--- client/src/components/links/index.tsx
+++ client/src/components/links/index.tsx
@@ -1,24 +1,2 @@
-import { links } from "@/utils";
-import type { FC } from "react";
-import { Link } from "wouter";
-
-export const Links: FC = () => (
-	<ul className="flex justify-center items-center max-[1000px]:w-full max-[1000px]:inline max-[1000px]:text-4xl max-[1000px]:text-center max-[1000px]:text-white">
-		{links.map(({ id, text, link }) => (
-			<li
-				key={id}
-				className="mx-2 max-[1000px]:mb-10 max-[1000px]:text-inherit"
-			>
-				<Link
-					href={link}
-					className="hover:opacity-70 max-[1000px]:flex max-[1000px]:justify-between max-[1000px]:items-center"
-				>
-					{text}
-					<span className="hidden max-[1000px]:inline max-[1000px]:text-yellow-200">
-						{">"}
-					</span>
-				</Link>
-			</li>
-		))}
-	</ul>
-);
+export { LinksNavbar } from "./navbar";
+export { LinksDashboard } from "./dashboard";
